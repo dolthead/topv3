ad3dfa56efb80a1e9fdb65bf3037abc4dcf4bea6
Todd H <github@simplybest.com>
2021-01-10 18:30:44 -0700

    add About screen

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,13 +2,10 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar color="primary">
-        <ion-title>Topzee</ion-title>
+        <ion-title>About Topzee</ion-title>
         <ion-buttons slot="end">
-          <ion-button @click="openAbout">
-            <ion-icon
-              slot="icon-only"
-              :icon="informationCircleOutline"
-            ></ion-icon>
+          <ion-button @click="closeAbout">
+            <ion-icon slot="icon-only" :icon="closeOutline"></ion-icon>
           </ion-button>
         </ion-buttons>
       </ion-toolbar>
@@ -17,28 +14,16 @@
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Topzee</ion-title>
+          <ion-title size="large">About Topzee</ion-title>
         </ion-toolbar>
       </ion-header>
-
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>
-          Start with Ionic
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://ionicframework.com/docs/components"
-            >UI Components</a
-          >
-        </p>
-      </div>
+      <div id="container">About</div>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { informationCircleOutline } from "ionicons/icons";
+import { closeOutline } from "ionicons/icons";
 import {
   IonContent,
   IonHeader,
@@ -51,10 +36,8 @@ import {
   modalController,
 } from "@ionic/vue";
 import { defineComponent } from "vue";
-import About from './About.vue';
-
 export default defineComponent({
-  name: "Home",
+  name: "About",
   components: {
     IonContent,
     IonHeader,
@@ -66,19 +49,12 @@ export default defineComponent({
     IonIcon,
   },
   setup() {
-    const openAbout = async () => {
-      const modal = await modalController.create({
-          component: About,
-          cssClass: 'my-custom-class',
-          componentProps: {
-            title: 'About'
-          },
-        });
-      return modal.present();
+    const closeAbout = () => {
+      modalController.dismiss();
     };
     return {
-      openAbout,
-      informationCircleOutline,
+      closeOutline,
+      closeAbout,
     };
   },
 });
@@ -87,28 +63,22 @@ export default defineComponent({
 <style scoped>
 #container {
   text-align: center;
-
   position: absolute;
   left: 0;
   right: 0;
   top: 50%;
   transform: translateY(-50%);
 }
-
 #container strong {
   font-size: 20px;
   line-height: 26px;
 }
-
 #container p {
   font-size: 16px;
   line-height: 22px;
-
   color: #8c8c8c;
-
   margin: 0;
 }
-
 #container a {
   text-decoration: none;
 }
